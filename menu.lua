7b3f1c139d7c142245c38780ca803c3236e0bbda
SSIO <oezdemir@can-mur.at>
2024-11-04 23:30:58 +0100

    Build Menu

--- menu.lua
+++ menu.lua
@@ -1,4 +1,4 @@
-local MenuSize = vec2(600, 350)
+local MenuSize = vec2(500, 300)
 local MenuStartCoords = vec2(500, 500)
 local TabsBarWidth = 0
 local SectionChildWidth = MenuSize.x - TabsBarWidth
@@ -8,21 +8,31 @@ local MachoPaneGap = 10
 local EachSectionWidth = (SectionChildWidth - (SectionsPadding * (SectionsCount + 1))) / SectionsCount
 local SectionOneStart = vec2(TabsBarWidth + (SectionsPadding * 1) + (EachSectionWidth * 0), SectionsPadding + MachoPaneGap)
 local SectionOneEnd = vec2(SectionOneStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+
+local SectionTwoStart = vec2(TabsBarWidth + (SectionsPadding * 2) + (EachSectionWidth * 1), SectionsPadding + MachoPaneGap)
+local SectionTwoEnd = vec2(SectionTwoStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+
+local SectionThreeStart = vec2(TabsBarWidth + (SectionsPadding * 3) + (EachSectionWidth * 2), SectionsPadding + MachoPaneGap)
+local SectionThreeEnd = vec2(SectionThreeStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+
 MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
 
 MachoMenuSetAccent(MenuWindow, 137, 52, 235)
 
-
+-- First tab
 FirstSection = MachoMenuGroup(MenuWindow, "Section One", SectionOneStart.x, SectionOneStart.y, SectionOneEnd.x, SectionOneEnd.y)
-
 MachoMenuButton(FirstSection, "Close", function()
-    MachoMenuDestroy(MenuWindow)
-end)
-
-
-MachoMenuButton(FirstSection, "GetResourceName", function()
     print(_G)
 end)
 
---local Payload = MachoWebRequest("https://raw.githubusercontent.com/MCONKN/MachoMenu/main/menu.lua")
---MachoIsolatedInject(Payload)
+-- Second tab
+SecondSection = MachoMenuGroup(MenuWindow, "Section Two", SectionTwoStart.x, SectionTwoStart.y, SectionTwoEnd.x, SectionTwoEnd.y)
+MachoMenuButton(SecondSection, "Close", function()
+    MachoMenuDestroy(MenuWindow)
+end)
+
+-- Third tab
+ThirdSection = MachoMenuGroup(MenuWindow, "Section Three", SectionThreeStart.x, SectionThreeStart.y, SectionThreeEnd.x, SectionThreeEnd.y)
+MachoMenuButton(ThirdSection, "Close", function()
+    MachoMenuDestroy(MenuWindow)
+end)
